9f9dcd3b80c7c7d15a0dbb5965b0c52d81f3ce6a
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2024-07-01 20:55:29 -0600

    Level

---
+++ Labs/Lab08_terrain_with_tiled/Lab08_terrain_with_tiled/terrain16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain16" tilewidth="16" tileheight="16" tilecount="528" columns="22">
+ <image source="../../../../UCR/2024 Cursos 1 Ciclo/Creacion de videojuegos/resources/Sprites/FondoMapa/Final/Tiles.png" width="352" height="384"/>
+</tileset>
